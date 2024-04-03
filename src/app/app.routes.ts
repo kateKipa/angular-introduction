c82fb0d0d954db9339deb0436986dc25a5302e12
kateKipa <kiparissi.kate@gmail.com>
2024-04-03 13:54:43 +0300

    routing primer finished

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+import { ComponentInputExampleComponent } from './components/component-input-example/component-input-example.component';
+import { ForDirectiveExampleComponent } from './components/for-directive-example/for-directive-example.component';
 
 export const routes: Routes = [
     {path : 'event-bind-example', component: EventBindExampleComponent},
     {path : 'welcome', component: WelcomeComponent},
-    {path : "", redirectTo: '/welcome', pathMatch: 'full'}
+    {path : "", redirectTo: '/welcome', pathMatch: 'full'},
+    {path : "component-input-example", component : ComponentInputExampleComponent},
+    {path : 'for-directive-example', component : ForDirectiveExampleComponent}
 ];
