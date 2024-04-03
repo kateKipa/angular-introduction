ea40cba3eac7fa216df2d72091262994246bf993
kateKipa <kiparissi.kate@gmail.com>
2024-04-03 17:08:41 +0300

    fancy-app-menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,12 @@ import { PersonTableComponent } from './components/person-table/person-table.com
 import { Person } from './shared/interfaces/person';
 import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { ListGroupMenuComponent } from './components/list-group-menu/list-group-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterLink, 
-    RouterOutlet,
-    PersonTableComponent, EventBindExampleComponent],
+  imports: [ListGroupMenuComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
