fdd93307fc986a3d8b4109ab4de4cc25e204aced
kateKipa <kiparissi.kate@gmail.com>
2024-05-16 13:07:47 +0300

    user login

--- src/app/shared/interfaces/user.ts
+++ src/app/shared/interfaces/user.ts
@@ -4,5 +4,15 @@ export interface User {
     email: string
     password: string
 }
-
 //To σχημα των δεδομενων που θα στειλω στο backend
+
+
+export interface Credentials {
+    email: string
+    password: string
+}
+
+export interface LoggedInUser {
+    fullname: string
+    email: string
+}
